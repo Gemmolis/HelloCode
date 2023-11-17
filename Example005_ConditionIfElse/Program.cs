643d9fd0272268006269a9f0ac5968c6d1024d95
Nastya Savinova <savinovaag@gmail.com>
2023-11-17 23:16:04 +0300

    Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Nastya")
+{
+    Console.WriteLine("Здравствуйте, Анастасия!"); 
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+
